6962589437c26460b68a54de0b5ae8bd868e12ab
Nabil Azouz <nazouz@e3r2p7.1337.ma>
2024-10-14 16:04:05 +0100

    idk

---
+++ FTP Server/tools/script.sh
@@ -0,0 +1,35 @@
+# #!/bin/bash
+
+# useradd -m $FTP_USER
+
+# echo "$FTP_USER:$FTP_PASS" | chpasswd
+
+# echo $FTP_USER >> /etc/vsftp.userlist
+
+# mkdir -p /home/$FTP_USER/ftp/somefiles
+
+# chown "$FTP_USER:$FTP_USER" "/home/$FTP_USER/ftp/somefiles"
+
+# vsftpd
+
+#!/bin/sh
+
+adduser --home /wp "$FTP_USER" --disabled-password && printf "$FTP_PASS\n$FTP_PASS" | passwd "$FTP_USER";
+
+mkdir -p /var/run/vsftpd/empty;
+
+chown $FTP_USER:$FTP_USER /wp;
+
+echo "
+write_enable=YES
+pasv_enable=YES
+chroot_local_user=YES
+pasv_min_port=40000
+pasv_max_port=40100
+# pam_service_name=ftp
+allow_writeable_chroot=YES
+" >> /etc/vsftpd.conf;
+
+echo "$FTP_USER" > /etc/ftpusers;
+
+vsftpd
