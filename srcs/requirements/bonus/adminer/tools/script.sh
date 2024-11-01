f5ec9843ea85bc53ebf716014be6b55fd47e5ede
Nabil AZOUZ <nabilos.fb@gmail.com>
2024-11-01 13:38:00 -0400

    fixed nginx and adminer

--- srcs/requirements/bonus/adminer/tools/script.sh
+++ srcs/requirements/bonus/adminer/tools/script.sh
@@ -2,12 +2,8 @@
 
 curl -L -o /var/www/html/adminer.php https://www.adminer.org/latest.php
 
-# chown -R www-data:www-data /var/www/html/adminer.php
+cd /var/www/html
 
-# chmod 755 /var/www/html/adminer.php
-
-# cd /var/www/html
-
-# rm -rf index.html
+rm -rf index.html
 
 php -S 0.0.0.0:9001
